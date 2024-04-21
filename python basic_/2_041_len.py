9f3b8a4b8cb8d7663232e91e07d5ef9a2b95f87b
bbangcha <152943008+bbangcha@users.noreply.github.com>
2024-04-21 14:33:17 +0900

    Delete python basic_ directory

--- python basic_/2_041_len.py
+++
@@ -1,2 +0,0 @@
-userMsg = input('메세지 입력 : ')
-print('메시지 문자열 길이 : {}'.format(len(userMsg)))
--- python basic_/2_050_1.py
+++
@@ -1,7 +0,0 @@
-carSpeed = int(input('속도 입력 : '))
-
-if carSpeed <= 50:
-    print('안전속도 준수!!')
-else:
-    print('안전속도 위반!!')
-
